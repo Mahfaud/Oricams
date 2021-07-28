06f4b03f5f8677b630b469a3218d18a3992afb5c
Mahfaud <mahfaud.cherdoud@gmail.com>
2021-07-28 15:11:03 +0200

    Appel de l'API et ajouts des éléments HTML et classes bootstraps avec Javascript

--- front-end/script/home.js
+++ front-end/script/home.js
@@ -9,28 +9,24 @@ httpRequest.onreadystatechange = () => {
     if (httpRequest.readyState == 4) {
         var response = JSON.parse(httpRequest.responseText)
         for(var i = 0; i < response.length; i++) {
-            let link = document.createElement("a")
-            let newLink = camsContainer.appendChild(link)
+            let newLink = camsContainer.appendChild(document.createElement("a"))
             newLink.setAttribute("href", "./product.html?id=" + response[i]._id)
-            let div = document.createElement("div")
-            let card = newLink.appendChild(div)
-            card.classList.add("col", "card", "f-wrap", "m-3")
+            let card = newLink.appendChild(document.createElement("div"))
+            newLink.classList.add("col-12" , "col-md-5", "card", "f-wrap", "m-3")
 
-
-            let img = document.createElement("img")
-            let newImg = card.appendChild(img)
+            let newImg = card.appendChild(document.createElement("img"))
             newImg.setAttribute("src", response[i].imageUrl)
             newImg.setAttribute("alt", "Caméra Vintage " +  response[i]._id)
             newImg.classList.add("card-img-top")
 
             cardBody = card.appendChild(document.createElement("div"))
-            cardBody.classList.add("card-body")
+            cardBody.classList.add("card-body", "row")
 
             cardTitle = cardBody.appendChild(document.createElement("h5"))
-            cardTitle.classList.add("card-title")
+            cardTitle.classList.add("card-title" , "mr-auto")
             cardTitle.innerHTML = response[i].name
 
-            cardDescription = card.appendChild(document.createElement("p"))
+            cardDescription = cardBody.appendChild(document.createElement("p"))
             cardDescription.classList.add("card-text")
             price = String(response[i].price / 10000) + " €"
             cardDescription.innerHTML = price
--- front-end/script/product.js
+++ front-end/script/product.js
@@ -5,3 +5,43 @@ var httpRequest = new XMLHttpRequest()
 
 httpRequest.open("GET", "http://localhost:3000/api/cameras/" + id, true)
 httpRequest.send()
+
+var product = document.getElementById("product")
+
+httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState == 4) {
+        var response = JSON.parse(httpRequest.responseText)
+
+        productImg = product.appendChild(document.createElement("img"))
+        productImg.classList.add("row", "card-header")
+        productImg.setAttribute("src", response.imageUrl)
+        productImg.setAttribute("alt", "Caméra vintage " + response.name)
+
+        cardBody = product.appendChild(document.createElement("div"))
+        cardBody.classList.add("card-body")
+
+        cardTitle = cardBody.appendChild(document.createElement("h3"))
+        cardTitle.innerHTML = response.name
+
+        cardPrice = cardBody.appendChild(document.createElement("h4"))
+        cardPrice.innerHTML = String(response.price / 10000) + " €"
+        cardPrice.classList.add("card-text")
+
+        cardDescription = cardBody.appendChild(document.createElement("p"))
+        cardDescription.innerHTML = response.description
+        cardDescription.classList.add("card-text")
+        
+        productLenses = cardBody.appendChild(document.createElement("select"))
+        productLenses.classList.add("form-control")
+        for (let i = 0; i < response.lenses.length; i++) {
+            let options = productLenses.appendChild(document.createElement("option"))
+            options.innerHTML = response.lenses[i]
+        }
+
+        cardButton = cardBody.appendChild(document.createElement("button"))
+        cardButton.innerHTML = "Ajouter au panier"
+        cardButton.setAttribute("type", "button")
+        cardButton.classList.add("btn", "btn-md", "bg-secondary", "text-light", "my-3", "ml-auto")
+
+    }
+}
